0d096ccf2d1ae49da3097d0216947208e42e16d3
shashank s <shashanksbhat03@gmail.com>
2024-09-08 10:20:56 +0530

    Resolved merge conflicts

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection(process.env.DATABASE_URL);
+const connection = mysql.createConnection(process.env.MYSQL_URL);
 
 connection.connect((err) => {
   if (err) {
